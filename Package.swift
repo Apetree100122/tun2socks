0cbdfc5a099b930317567350df55c50582e45011
Alexander Petree <apetree1001@email.phoenix.edu>
2025-04-19 02:02:01 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,21 +2,9 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
-
-let package = Package(
-    name: "tun2socks",
-    products: [
-        .library(
-            name: "tun2socks",
-            targets: ["tun2socks"]
-        ),
-    ],
-    dependencies: [],
-    targets: [
-        .target(
-            name: "tun2socks",
-            dependencies: ["lwip"]
-        ),
+let package = Package( name: "tun2socks", products: [  .library(  name: "tun2socks",  targets: ["tun2socks"]),
+    ], dependencies: [], targets: [
+        .target(  name: "tun2socks", dependencies: ["lwip"] ),
         .target(
             name: "lwip"
         ),
